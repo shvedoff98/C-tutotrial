c525df148030140015f01ccea124c0d14d8bb972
Kirill Shvedov <74286567+shvedoff98@users.noreply.github.com>
2021-03-09 15:48:40 +0300

    Add files via upload

--- lambda.cpp
+++ lambda.cpp
@@ -1,27 +1,25 @@
-#include <algorithm>
-#include <array>
 #include <iostream>
-#include <string_view>
+#include <string>
+
+using namespace std;
+
+auto l = [] { cout << "Hello lambda" << "\n"; };
+auto k = [] (const string& s ) -> string { cout << s << "\n"; return s; };
 
 int main()
 {
-    std::array<std::string_view,4> arr{"apple","banana","walnut","lemon"};
+    int x = 0, y = 42;
+    auto qqq = [x, &y] { cout << "x: " << x << "\n";
+                         cout << "y: " << y << "\n";
+                         ++y;
+                       };
+    x = y = 77;
+    qqq();
+    qqq();
+    cout << "final y: " << y << "\n";
 
-    auto found{ std::find_if(arr.begin(),arr.end(),
-                            [](std::string_view str)
-                            {
-                                return(str.find("nut") != std::string_view::npos);
-                            }) };
-
-
-    if (found == arr.end())
-    {
-        std::cout << "No nuts\n";
-    }
-    else 
-    {
-        std::cout << "Found " << *found << '\n';
-    }
-
+    k("Fucking fuck");
+    l();
     return 0;
+
 }
