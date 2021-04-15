9549707a0f9e28525705df5babf8a467e9392a44
Kirill Shvedov <74286567+shvedoff98@users.noreply.github.com>
2021-04-15 12:37:54 +0300

    Add files via upload

--- basics/test_excp.cpp
+++ basics/test_excp.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
 using namespace std;
 
+void skip_to_int();
+int get_int();
+int get_int(int low, int high);
+
+int get_int()
+{
+    int n = 0;
+    while (true)
+    {
+        if (cin >> n) return n;
+        cout << "It's not a number! Try again!\n";
+        skip_to_int();
+    }
+}
+
+int get_int(int low, int high)
+{
+    cout << "Enter the integer number from "
+        << low << " to " << high << " included: \n";
+    while (true)
+    {
+        int n = get_int();
+        if (low <= n && n <= high) return n;
+        cout << "Sorry, "  
+            << "out-of-range error... try again!\n";
+    }
+}
+
 void skip_to_int()
 {
     if (cin.fail())
@@ -32,8 +60,13 @@ int main(int argc, char* argv[])
             cout << "Out-of-range error bound...\n";
         }
         else {
-            cout << "Sorry, it is not a number!\n Try again!\n";
-            skip_to_int();
+            get_int();
         }
     }
+
+    int x = get_int(1, 10);
+    cout << "x: " << x << "\n";
+
+    int z = get_int(2, 300);
+    cout << "z: " << z << "\n";
 }
